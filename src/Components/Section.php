4918e9f8f15023658a4c3da9ed54ff6aade098ee
Qiwen Zhu <qiwzhu@iu.edu>
2024-10-31 13:43:26 -0400

    Section takes array of objects of either Chunk, or BaseNode

--- src/Components/Section.php
+++ src/Components/Section.php
@@ -2,13 +2,18 @@
 
 namespace Edu\IU\WSB\IUWebFrameworkComponents\Components;
 
+use Edu\IU\RSB\StructuredDataNodes\BaseNode;
 use Edu\IU\RSB\StructuredDataNodes\GroupNode;
 use Edu\IU\RSB\StructuredDataNodes\Text\DropdownNode;
 use Edu\IU\RSB\StructuredDataNodes\Text\TextInputNode;
+use Edu\IU\WSB\IUWebFrameworkComponents\Components\Chunks\Chunk;
 
 class Section extends Component {
 
 
+    /**
+     * @param array $chunkNodes, array of objects of Chunk, or BaseNode
+     */
     public function __construct(array $chunkNodes)
     {
         $this->dataNode = new GroupNode('section');
@@ -19,8 +24,17 @@ class Section extends Component {
         $detailsNode = new GroupNode('details');
         $this->dataNode->addChild($detailsNode);
 
+
+
         foreach ($chunkNodes as $chunkNode){
-            $this->dataNode->addChild($chunkNode);
+            if ($chunkNode instanceof Chunk){
+                $this->dataNode->addChild($chunkNode->toDataNode());
+            }elseif ($chunkNode instanceof BaseNode){
+                $this->dataNode->addChild($chunkNode);
+            }else{
+                throw new \RuntimeException('Chunks array has be contain object of Chunk, or BaseNode');
+            }
+
         }
     }
 
